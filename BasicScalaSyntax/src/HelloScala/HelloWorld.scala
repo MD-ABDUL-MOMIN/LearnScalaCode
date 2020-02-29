0ca19c091d51c6b5de32e1d610a64259cf65d49d
MD-ABDUL-MOMIN <momin.ice16@gmail.com>
2020-02-29 20:00:49 +0600

    Hello program added

---
+++ BasicScalaSyntax/src/HelloScala/HelloWorld.scala
@@ -0,0 +1,11 @@
+package HelloScala
+
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    println("Hello Scala!")
+
+  }
+
+
+}
---
+++ BasicScalaSyntax/src/HelloScala/OperationInScala.scala
@@ -0,0 +1,5 @@
+package HelloScala
+
+object OperationInScala {
+
+}
